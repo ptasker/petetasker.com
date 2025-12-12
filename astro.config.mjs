455c3cd7a506252715450fbf49f063d4ca472b58
Peter Tasker <ptasker@github.com>
2025-12-12 16:14:39 -0500

    SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,11 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind(), mdx()],
+  integrations: [react(), tailwind(), mdx(), sitemap()],
   site: 'https://petetasker.com',
   markdown: {
     shikiConfig: {
